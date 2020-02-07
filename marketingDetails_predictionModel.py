6968a7e28adab0488cef6a9dbd989b45099d2e34
Vishal Vinayak Mujumdar <vmujumdar2@sapient.com>
2020-02-07 15:21:37 +0400

    Prediction Model for marketing details uploaded to GitHub

--- marketingDetails_predictionModel.py
+++ marketingDetails_predictionModel.py
@@ -6,9 +6,9 @@ Created on Sat Feb  1 18:41:58 2020
 """
 
 import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
+#import numpy as np
+#import matplotlib.pyplot as plt
+#import seaborn as sns
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 
